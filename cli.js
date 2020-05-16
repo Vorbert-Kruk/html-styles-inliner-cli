5468f81bc25c44172e423c995cdb95e278486d5d
Vorbert Kruk <pryrokopter@gmail.com>
2020-05-16 19:50:36 +0200

    Added html parser and extracted href from link elements

--- cli.js
+++ cli.js
@@ -1,13 +1,29 @@
 #!/usr/bin/env node
 const { getArgValue } = require('./args');
-const { getAbsoluteFilePath } = require('./utils');
-const { params } = require('./consts');
+const { getAbsoluteFilePath, hrefRegex } = require('./utils');
+const { params, fileExtensions } = require('./consts');
 const { fileExists, getFileData } = require('./file-utils');
+const { parse } = require('node-html-parser');
 
 const inputFile = getAbsoluteFilePath(getArgValue(params.input));
 const outputFile = getAbsoluteFilePath(getArgValue(params.output));
 
-getFileData(inputFile).then(data => console.log(data));
+getFileData(inputFile).then(html => {
+  const htmlDocument = parse(html);
+  const linkElements = htmlDocument.querySelectorAll('link');
+  if (!linkElements && linkElements.length === 0) return;
+
+  const stylePaths = linkElements
+    .filter(linkElement => linkElement.rawAttrs.includes('stylesheet'))
+    .map(linkElement =>
+      linkElement.rawAttrs.substring(
+        linkElement.rawAttrs.indexOf('href'),
+        linkElement.rawAttrs.indexOf(fileExtensions.css) + fileExtensions.css.length,
+      ),
+    );
+
+  console.log(stylePaths);
+});
 
 console.log('Nie wydupcyłem się c:');
 
--- utils.js
+++ utils.js
@@ -2,7 +2,9 @@ const _path = require('path');
 const currentWorkingDirectory = process.cwd();
 
 const getAbsoluteFilePath = filePath => _path.join(currentWorkingDirectory, filePath);
+const hrefRegex = /<a\s+(?:[^>]*?\s+)?href=(["'])(.*?)\1/;
 
 module.exports = {
   getAbsoluteFilePath,
+  hrefRegex,
 };
